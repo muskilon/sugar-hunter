3d7c8efb39044d8c03f17b5d9ca7c15f2668be7a
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-09 19:42:55 +0300

    Country Фрагмент, вьюмоделька, лайаут

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/place/country/CountryViewModel.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.ui.filter.place.country
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CountryViewModel : ViewModel() {
+
+    val mutable = MutableLiveData<Int>()
+
+}
