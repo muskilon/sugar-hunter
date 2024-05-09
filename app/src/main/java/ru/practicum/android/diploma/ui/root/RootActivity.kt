7b8a57f56c2beaf229f84e12511eeb48ed758a37
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-09 14:26:21 +0300

    Создала RootActivity, RootViewModel, bottomNav

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,24 +1,22 @@
 package ru.practicum.android.diploma.ui.root
 
-import android.os.Binder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
-class RootActivity : AppCompatActivity() {
+class RootActivity: AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
-    private val viewModel by viewModel<RootViewModel>()
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
+   // private val viewModel by viewModel<RootViewModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRootBinding.inflate(layoutInflater)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
@@ -34,9 +32,17 @@ class RootActivity : AppCompatActivity() {
 
     val navHostFragment =
         supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
-    /*val navController = navHostFragment.navController
+   /* val navController = navHostFragment.navController
     val bottomNavigationView = binding.bottomNavigationView
     bottomNavigationView.setupWithNavController(navController)*/
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
+
+
 
--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootViewModel.kt
@@ -1,6 +1,10 @@
 package ru.practicum.android.diploma.ui.root
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RootViewModel: ViewModel() {
+
+    val mutable = MutableLiveData<Int>()
+
 }
