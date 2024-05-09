fc477d20bdbde244a9f364baac7667a8546d0fce
Practicum <practicum@ya.ru>
2024-05-09 13:04:02 +0300

    Revert "Подключил Koin в App и добавил папки"
    This reverts commit 268c27d2661c50506db46c7a17af6b43662973b1.

--- app/src/main/java/ru/practicum/android/diploma/app/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/App.kt
@@ -1,19 +1,10 @@
 package ru.practicum.android.diploma.app
 
 import android.app.Application
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-import ru.practicum.android.diploma.di.dataModules
-import ru.practicum.android.diploma.di.domainModules
-import ru.practicum.android.diploma.di.viewModelModules
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin{
-            androidContext(this@App)
-            modules(dataModules, domainModules, viewModelModules)
-        }
 
     }
 }
