5467daf95cb4d282f1b91dda31299e4b158876f5
Denis <denis.spb.2004@gmail.com>
2024-05-09 17:35:24 +0300

    GH-10 - fix 5

--- app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class TeamFragment : Fragment() {
 
