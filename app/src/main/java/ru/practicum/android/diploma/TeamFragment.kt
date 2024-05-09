3fabe7e8a2d364f44a1f11dad3a86ed58666c745
Denis <denis.spb.2004@gmail.com>
2024-05-09 17:23:40 +0300

    GH-10 - fix 3

--- app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
@@ -1,12 +1,9 @@
 package ru.practicum.android.diploma
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
 class TeamFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
