86387fb4cd42858e6e771d7573c0d3110e031e78
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-09 13:40:12 +0300

    Создала RootActivity, RootViewModel, bottomNav

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,14 +1,25 @@
 package ru.practicum.android.diploma.ui.root
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+    private val viewModel by viewModel<RootViewModel>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
@@ -18,4 +29,14 @@ class RootActivity : AppCompatActivity() {
         // ...
     }
 
+   /* binding.bottomNavigationView.menu.findItem(R.id.mediaLibraryFragment).isChecked = true
+    viewModel.updateTheme()*/
+
+    val navHostFragment =
+        supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+    /*val navController = navHostFragment.navController
+    val bottomNavigationView = binding.bottomNavigationView
+    bottomNavigationView.setupWithNavController(navController)*/
+
 }
+
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootViewModel.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.ui.root
+
+import androidx.lifecycle.ViewModel
+
+class RootViewModel: ViewModel() {
+}
