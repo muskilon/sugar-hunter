213affebf21c66a6d32f52467c684e3823d7dd59
FantasmasDe <fantasmas.dev@gmail.com>
2024-05-10 20:47:34 +0300

    1. Расширен список размеров 2. Раставалены строковые ресурсы 3. Доработана тема klyakssa 2. Доработан метод смены темы muskilon 4. Общее причёсывание фрагментов

--- app/src/main/java/ru/practicum/android/diploma/app/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/App.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.app
 
 import android.app.Application
-import android.content.res.Configuration
-import android.content.res.Resources
 import androidx.appcompat.app.AppCompatDelegate
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -17,40 +15,10 @@ class App : Application() {
             androidContext(this@App)
             modules(dataModules, domainModules, viewModelModules)
         }
-        appResource = resources
 
-        setAppTheme()
-
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        )
     }
 
-    private fun setAppTheme() {
-        when (this.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                changeTheme(true)
-            }
-
-            Configuration.UI_MODE_NIGHT_NO -> {
-                changeTheme(false)
-            }
-
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                changeTheme(false)
-            }
-        }
-    }
-
-    companion object {
-        private var appResource: Resources? = null
-
-        fun changeTheme(isNightMode: Boolean) {
-            when (isNightMode) {
-                true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-        }
-
-        fun getAppResource(): Resources? { // Это если где-то нужно будет получить ресурсы
-            return appResource
-        }
-    }
 }
