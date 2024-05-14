f758bbeebe9d118a560edadcf5e032a0e2aee0fc
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-14 12:56:59 +0300

    Создала и наполнила имплементацию базы данных и сопутствующие интерфесы и классы, добавила в дао необходжимый метод

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/db/FavouriteDataBaseInteractorImpl.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.domain.db
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Vacancy
+
+class FavouriteDataBaseInteractorImpl(private val favouriteDataBaseRepository: FavouriteDataBaseRepository) :
+    FavouriteDataBaseInteractor {
+    override suspend fun addFavouriteVacancy(vacancy: Vacancy) {
+        favouriteDataBaseRepository.addFavouriteVacancy(vacancy)
+    }
+
+    override suspend fun deleteFavouriteVacancy(vacancy: Vacancy) {
+        favouriteDataBaseRepository.deleteFavouriteVacancy(vacancy)
+    }
+
+    override suspend fun checkIdInFavourites(id: String): Boolean {
+        return favouriteDataBaseRepository.checkIdInFavourites(id)
+    }
+
+    override fun getFavouritesVacancies(): Flow<List<Vacancy>> {
+        return favouriteDataBaseRepository.getFavouritesVacancies()
+    }
+
+}
