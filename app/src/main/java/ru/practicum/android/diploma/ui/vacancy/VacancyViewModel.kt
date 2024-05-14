2d58ee6338278eead0406370c0d0840b0dc51de2
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-14 13:25:04 +0300

    создала VacancyViewModel

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
@@ -1,7 +1,10 @@
 package ru.practicum.android.diploma.ui.vacancy
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.db.FavouriteDataBaseInteractor
 import ru.practicum.android.diploma.domain.models.Vacancy
 
@@ -10,6 +13,31 @@ class VacancyViewModel(
     private val favouriteDataBaseInteractor: FavouriteDataBaseInteractor
 ) : ViewModel() {
 
-    val mutable = MutableLiveData<Int>()
+    private var checkInFavouritesMutableLiveData = MutableLiveData<Boolean>()
+    fun checkInFavourites(): LiveData<Boolean> = checkInFavouritesMutableLiveData
+
+    init {
+        viewModelScope.launch {
+            checkIdInFavourites(vacancy)
+        }
+    }
+
+    suspend fun checkIdInFavourites(vacancy: Vacancy) {
+        viewModelScope.launch {
+            checkInFavouritesMutableLiveData.postValue(favouriteDataBaseInteractor.checkIdInFavourites(vacancy.id))
+        }
+    }
+
+    suspend fun addFavouriteVacancy(vacancy: Vacancy) {
+        checkIdInFavourites(vacancy)
+        favouriteDataBaseInteractor.addFavouriteVacancy(vacancy)
+    }
+
+    suspend fun deleteFavouriteVacancy(vacancy: Vacancy) {
+        checkIdInFavourites(vacancy)
+        favouriteDataBaseInteractor.deleteFavouriteVacancy(vacancy)
+    }
 
 }
+
+
