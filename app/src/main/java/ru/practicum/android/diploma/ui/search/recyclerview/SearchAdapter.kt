429d8483919145d7d651983ecee6707596279ddd
FantasmasDe <fantasmas.dev@gmail.com>
2024-05-15 23:36:25 +0300

    двойной рекуклер, исправле главный экран откуда тут пустые импорты

--- app/src/main/java/ru/practicum/android/diploma/ui/search/recyclerview/SearchAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/recyclerview/SearchAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.databinding.ItemEmptyBinding
 import ru.practicum.android.diploma.databinding.ItemVacancyBinding
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.util.DiffUtilCallback
@@ -14,15 +15,39 @@ class SearchAdapter(
 
     private val vacancy = ArrayList<Vacancy>()
 
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            0
+        } else {
+            1
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val layoutInspector = LayoutInflater.from(parent.context)
-        return SearchViewHolder(ItemVacancyBinding.inflate(layoutInspector, parent, false))
+        return when (viewType) {
+            0 -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                SearchViewHolder.EmptyViewHolder(ItemEmptyBinding.inflate(layoutInflater, parent, false))
+            }
+
+            1 -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                SearchViewHolder.VacancyViewHolder(ItemVacancyBinding.inflate(layoutInflater, parent, false))
+            }
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        holder.bind(vacancy[position])
-        holder.itemView.setOnClickListener {
-            onItemClick.invoke(vacancy[holder.adapterPosition])
+        when (holder) {
+            is SearchViewHolder.EmptyViewHolder -> { }
+            is SearchViewHolder.VacancyViewHolder -> {
+                holder.bind(vacancy[position - 1])
+                holder.itemView.setOnClickListener {
+                    onItemClick.invoke(vacancy[holder.adapterPosition])
+                }
+            }
         }
     }
 
