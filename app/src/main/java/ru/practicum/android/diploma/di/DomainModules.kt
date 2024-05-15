6f0c151e80e14042b3ef080e74825b81303860e9
RobotComp.ru <luchik.sakuras@yandex.ru>
2024-05-15 18:48:46 +0300

    fix errorри переходе на экран вакансия

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModules.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModules.kt
@@ -4,6 +4,8 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.data.db.FavouriteDataBaseRepositoryImpl
 import ru.practicum.android.diploma.domain.VacanciesInterActor
 import ru.practicum.android.diploma.domain.VacanciesRepository
+import ru.practicum.android.diploma.domain.db.FavouriteDataBaseInteractor
+import ru.practicum.android.diploma.domain.db.FavouriteDataBaseInteractorImpl
 import ru.practicum.android.diploma.domain.db.FavouriteDataBaseRepository
 import ru.practicum.android.diploma.domain.impl.VacanciesInterActorImpl
 import ru.practicum.android.diploma.domain.impl.VacanciesRepositoryImpl
@@ -11,5 +13,6 @@ import ru.practicum.android.diploma.domain.impl.VacanciesRepositoryImpl
 val domainModules = module {
     factory<VacanciesInterActor> { VacanciesInterActorImpl(repository = get()) }
     single<VacanciesRepository> { VacanciesRepositoryImpl(networkClient = get()) }
-    single<FavouriteDataBaseRepository> { FavouriteDataBaseRepositoryImpl(get(), get()) }
+    single<FavouriteDataBaseRepository> { FavouriteDataBaseRepositoryImpl(database = get(), convertor = get()) }
+    single<FavouriteDataBaseInteractor> { FavouriteDataBaseInteractorImpl(favouriteDataBaseRepository = get()) }
 }
