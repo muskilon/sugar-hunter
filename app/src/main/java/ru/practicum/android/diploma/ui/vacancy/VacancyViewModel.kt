53557ef75ce109ce479b0fb9168553f0b45a164d
FantasmasDe <fantasmas.dev@gmail.com>
2024-05-17 23:24:02 +0300

    Доделан экран Вакансия, бд нот ворк

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
@@ -1,41 +1,102 @@
+
 package ru.practicum.android.diploma.ui.vacancy
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.practicum.android.diploma.domain.VacanciesInterActor
 import ru.practicum.android.diploma.domain.db.FavouriteDataBaseInteractor
+import ru.practicum.android.diploma.domain.models.Resource
 import ru.practicum.android.diploma.domain.models.VacancyDetails
+import ru.practicum.android.diploma.ui.vacancy.models.VacancyFragmentState
 
 class VacancyViewModel(
-    private val vacancy: VacancyDetails,
-    private val favouriteDataBaseInteractor: FavouriteDataBaseInteractor
+    private val vacancyId: String,
+    private val favouriteDataBaseInteractor: FavouriteDataBaseInteractor,
+    private val vacanciesInterActor: VacanciesInterActor
 ) : ViewModel() {
 
     private var checkInFavouritesMutableLiveData = MutableLiveData<Boolean>()
     fun checkInFavouritesLiveData(): LiveData<Boolean> = checkInFavouritesMutableLiveData
 
+    private var vacancyScreenState = MutableLiveData<VacancyFragmentState>(VacancyFragmentState.Start)
+    fun getVacancyScreenStateLiveData(): LiveData<VacancyFragmentState> = vacancyScreenState
+
     init {
         viewModelScope.launch {
-            checkIdInFavourites(vacancy)
+            searchVacancyById(vacancyId)
+            checkIdInFavourites(vacancyId)
         }
     }
 
-    suspend fun checkIdInFavourites(vacancy: VacancyDetails) {
+    fun searchVacancyById(vacancyId: String) {
         viewModelScope.launch {
-            checkInFavouritesMutableLiveData.postValue(favouriteDataBaseInteractor.checkIdInFavourites(vacancy.id))
+            vacancyScreenState.postValue(VacancyFragmentState.Loading)
+            vacanciesInterActor
+                .getVacancy(vacancyId)
+                .collect { result ->
+                    processResult(result)
+                }
+
         }
     }
 
-    suspend fun addFavouriteVacancy(vacancy: VacancyDetails) {
-        checkIdInFavourites(vacancy)
-        favouriteDataBaseInteractor.addFavouriteVacancy(vacancy)
+    fun processResult(foundVacancy: Resource<VacancyDetails>) {
+        when (foundVacancy) {
+            is Resource.ConnectionError -> {
+                vacancyScreenState.postValue(VacancyFragmentState.Error)
+            }
+
+            is Resource.NotFound -> {
+                vacancyScreenState.postValue(VacancyFragmentState.Empty)
+            }
+
+            is Resource.Data -> {
+                vacancyScreenState.postValue(VacancyFragmentState.Content(foundVacancy.value as VacancyDetails))
+            }
+        }
     }
 
-    suspend fun deleteFavouriteVacancy(vacancy: VacancyDetails) {
-        checkIdInFavourites(vacancy)
-        favouriteDataBaseInteractor.deleteFavouriteVacancy(vacancy)
+    suspend fun checkIdInFavourites(id: String) {
+        withContext(Dispatchers.IO) {
+            checkInFavouritesMutableLiveData.postValue(favouriteDataBaseInteractor.checkIdInFavourites(id))
+        }
     }
 
+    suspend fun likeOrDislikeButton() {
+        if (checkInFavouritesMutableLiveData.value == true) {
+            addFavouriteVacancy()
+        } else {
+            deleteFavouriteVacancy()
+        }
+    }
+
+    suspend fun addFavouriteVacancy() {
+        if (vacancyScreenState.value is VacancyFragmentState.Content) {
+            Log.d("add", "y")
+            withContext(Dispatchers.IO) {
+                favouriteDataBaseInteractor.addFavouriteVacancy(
+                    (vacancyScreenState.value as VacancyFragmentState.Content).vacancy
+                )
+                checkInFavouritesMutableLiveData.postValue(true)
+            }
+        }
+    }
+
+    suspend fun deleteFavouriteVacancy() {
+        if (vacancyScreenState.value is VacancyFragmentState.Content) {
+            Log.d("add", "n")
+            withContext(Dispatchers.IO) {
+                favouriteDataBaseInteractor.deleteFavouriteVacancy(
+                    (vacancyScreenState.value as VacancyFragmentState.Content).vacancy
+                )
+                checkInFavouritesMutableLiveData.postValue(false)
+            }
+        }
+    }
 }
