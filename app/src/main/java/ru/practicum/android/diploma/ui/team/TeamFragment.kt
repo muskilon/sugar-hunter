f0787e96d1de800e424539c3b145bf593040735d
Elon Musk <s.morbis@gmail.com>
2024-06-03 16:06:59 +0300

    почистил navgraph и верстку

--- app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
-
 class TeamFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
